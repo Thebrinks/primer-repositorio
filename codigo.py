8508ce4cc2fa3089aa70e54e6a09f3dc1fab7dfd
Thebrinks <jpfmblackwhite@gmail.com>
2026-05-22 19:04:01 -0500

    Add second surname to greeting message

--- codigo.py
+++ codigo.py
@@ -1,6 +1,7 @@
 nombre="Juan"
 appellido="Fuquen"
+segundo_apellido="Martinez"
 
-saludo ="Hola " + "soy" + nombre + " " + appellido + "¿Como estas?"
+saludo ="Hola " + "soy" + nombre + " " + appellido + " " + segundo_apellido + "¿Como estas?"
 
 print(saludo)
